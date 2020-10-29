a7b4cce6dc1d1e1de6a0977bf08feb04f58d8bba
Colin McFadden <cmcfadden@gmail.com>
2020-10-29 11:06:52 -0500

    Fix el binding so that we can bind refs that may be reactive

--- example/App.vue
+++ example/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="container">
-    <VueFirework></VueFirework>
-
+    <VueFirework :el="button"></VueFirework>
     <div class="intro">
       <h1>To use the firework component developed by vue, click on the component.</h1>
-      <p>Try clicking on the screen</p>
+      <p><button ref="mybutton">Click me!</button></p>
     </div>
-
     <h2>🔥props</h2>
     <li>[el].   The element that needs to bind firework effects.</li>
     <li>[fireCanvasStyle].   Configuration of firework left and top properties.</li>
@@ -23,6 +21,26 @@ import VueFirework from '../src/firework.vue';
 
 export default {
   components: { VueFirework },
+  data() {
+    return {
+      button: null,
+    };
+  },
+  methods: {
+    updateRefButton() {
+      if (this.$refs.mybutton) {
+        this.button = this.$refs.mybutton;
+      } else {
+        this.button = null;
+      }
+    },
+  },
+  updated() {
+    this.updateRefButton();
+  },
+  mounted() {
+    this.updateRefButton();
+  },
 };
 </script>
 
